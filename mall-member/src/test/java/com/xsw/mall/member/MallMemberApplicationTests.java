70111ef43fd91f669cf778081bd4ac2435806a6c
xsw <xsw@atguigu.com>
2022-09-08 11:12:49 +0800

    项目初始结构创建

---
+++ mall-member/src/test/java/com/xsw/mall/member/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsw.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
